ca1e3d92f5890aa973d3594da8b3dd0697b20db5
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-20 19:08:14 +0700

    Add button to Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { openSidebar, openModal } = useGlobalContext();
+
+  return (
+    <main>
+      <button type="button" onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button type="button" className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
 };
 
 export default Home;
